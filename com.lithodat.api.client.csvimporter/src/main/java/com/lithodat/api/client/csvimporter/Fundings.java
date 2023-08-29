7ac3e93a5dd0c6f0c6d5afe72f9b2194e3a23c0f
Xinyan Zhang <xinyan.zhang@lithodat.com>
2023-08-29 21:54:25 +1000

    process number column in fundings

--- com.lithodat.api.client.csvimporter/src/main/java/com/lithodat/api/client/csvimporter/Fundings.java
+++ com.lithodat.api.client.csvimporter/src/main/java/com/lithodat/api/client/csvimporter/Fundings.java
@@ -14,34 +14,67 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Fundings {
 
-    private final String fundingUrl;
+	private final String fundingUrl;
 
-    public Fundings(String endpoint, String username, String password) {
+	public Fundings(String endpoint, String username, String password) {
 
 		super();
-        
+
 		this.fundingUrl = endpoint + "/api/core/fundings";
 
 	}
 
-    
-    public String getAllFundings(String token) throws Exception {
+	public JsonNode getAllFundings(String token) throws Exception {
 		HttpHeaders headers = new HttpHeaders();
 
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 		headers.setContentType(MediaType.APPLICATION_JSON);
 		headers.setBearerAuth(token);
 
-		ResponseEntity<String> result = new RestTemplate().exchange(fundingUrl, HttpMethod.GET, new HttpEntity<String>(headers), String.class);
+		ResponseEntity<String> result = new RestTemplate().exchange(fundingUrl, HttpMethod.GET,
+				new HttpEntity<String>(headers), String.class);
 
 		ObjectMapper objectMapper = new ObjectMapper();
 
 		JsonNode jsonNode = objectMapper.readTree(result.getBody());
 
-		return jsonNode.toString();
+		return jsonNode;
 	}
 
-    public static void main(String[] args) {
+	public void putAllFundings(String token, JsonNode fundings) throws Exception {
+		HttpHeaders headers = new HttpHeaders();
+
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		headers.setBearerAuth(token);
+
+		int sucessCount = 0;
+
+		for (JsonNode funding : fundings) {
+			// Convert the list of JsonNode to a single JSON string
+			String jsonString = funding.toString();
+
+			HttpEntity<String> entity = new HttpEntity<>(jsonString, headers);
+
+			ResponseEntity<String> result = new RestTemplate().exchange(fundingUrl, HttpMethod.PUT, entity,
+					String.class);
+
+			// Read response code and body
+			int responseCode = result.getStatusCodeValue();
+			String responseBody = result.getBody();
+
+			if (responseCode == 200) {
+				sucessCount++;
+			} else {
+				System.out.println("Error: " + responseCode + " " + responseBody);
+			}
+		}
+
+		System.out.println("Total number of fundings: " + fundings.size());
+		System.out.println("Total number of fundings successfully updated: " + sucessCount);
+	}
+
+	public static void main(String[] args) {
 		LithoAuth lithoAuth = new LithoAuth("https://testapp.lithodat.com", "kimberlyz", "Kimberly1234");
 		String authenticationKey = null;
 		try {
@@ -52,9 +85,11 @@ public class Fundings {
 		}
 		if (authenticationKey != null) {
 			try {
-                Fundings fundings = new Fundings("https://testapp.lithodat.com", "kimberlyz", "Kimberly1234");
-				String fundingStr = fundings.getAllFundings(authenticationKey);
-				System.out.println("Fundings: " + fundingStr);
+				Fundings fundings = new Fundings("https://testapp.lithodat.com", "kimberlyz", "Kimberly1234");
+				JsonNode fundingsJsonNode = fundings.getAllFundings(authenticationKey);
+				// Start processing funds
+				fundingsJsonNode = FundingProcessor.processJsonFile(fundingsJsonNode);
+				fundings.putAllFundings(authenticationKey, fundingsJsonNode);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
