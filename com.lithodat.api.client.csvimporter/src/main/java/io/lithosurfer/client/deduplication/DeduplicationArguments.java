0de0cfe14e77dd1cd4b564ba4554c8e050eb46e1
MoritzTheile <theile@mtheile.com>
2023-11-24 13:30:20 +0100

    finished deduplication

--- com.lithodat.api.client.csvimporter/src/main/java/io/lithosurfer/client/deduplication/DeduplicationArguments.java
+++ com.lithodat.api.client.csvimporter/src/main/java/io/lithosurfer/client/deduplication/DeduplicationArguments.java
@@ -1,13 +1,30 @@
 package io.lithosurfer.client.deduplication;
 
+import java.util.Arrays;
+
 import io.lithosurfer.client.LithoArguments;
 
 public class DeduplicationArguments extends LithoArguments {
 
+	public enum MODE {merge, report}
+	
+	private MODE mode;
+	
 	public DeduplicationArguments(String[] args) throws Exception {
-
 		super(args);
-		// more attributes could be handled here
+		try {
+			mode = MODE.valueOf(args [3]);
+		} catch (Exception e) {
+			System.out.println("third arg has to be one of "+Arrays.asList(MODE.values()));
+		}
 	}
+
+	public MODE getMode() {
+		return mode;
+	}
+
+
+	
+	
 
 }
